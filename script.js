77676d15c023f58c0f4dad97c18655f19ef84add
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-25 17:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here!
+const scrollContainer = document.getElementById("infi-list");
+let i = 10; // Start from 10 since you already added 10 items by default
 
+function addOneItem() {
+  const li = document.createElement("li");
+  li.innerText = `Item ${i++}`;
+  scrollContainer.appendChild(li);
+}
+
+// Add 10 items by default
+for (let j = 0; j < 10; j++) {
+  addOneItem();
+}
+
+// Add more items when the user reaches the end of the list
+scrollContainer.addEventListener("scroll", () => {
+  if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
+    for (let j = 0; j < 2; j++) {
+      addOneItem();
+    }
+  }
+});
